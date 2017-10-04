08f0b9224974fdfd77179642c69aed9019ab5797
horaciocoronel <arielc@gmail.com>
2017-10-04 15:06:55 -0400

    Exercise 3 - Add show 1 contact dinamic url | Add Styles to show_contact

--- crm.rb
+++ crm.rb
@@ -12,6 +12,20 @@ require 'sinatra'
     erb :contacts
   end
 
+  get '/contacts/:id' do
+  # params[:id] contains the id from the URL
+  @contact = Contact.find_by({id: params[:id].to_i})
+    if @contact
+      erb :show_contact
+    else
+      raise Sinatra::NotFound
+    end
+  end
+
+  not_found do
+  "'Sinatra doesn't know this ditty...'"
+  end
+
 after do
   ActiveRecord::Base.connection.close
 end
