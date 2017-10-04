d6f42281c2b7ecd62168425e85a0c01222767595
horaciocoronel <arielc@gmail.com>
2017-10-03 23:01:14 -0400

    List contacts - Add CSS

--- crm.rb
+++ crm.rb
@@ -1,6 +1,16 @@
 require_relative 'contact'
 require 'sinatra'
 
-get '/' do
-  erb :index
+  get '/' do
+    erb :index
+  end
+
+  get '/contacts' do
+    @contacts = Contact.all
+    # puts @contacts
+    erb :contacts
+  end
+
+after do
+  ActiveRecord::Base.connection.close
 end
