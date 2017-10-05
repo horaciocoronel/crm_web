0659291f2c09ac7b1e96a3a0198daafb71d934c7
horaciocoronel <arielc@gmail.com>
2017-10-05 16:03:16 -0400

    Add update and delete functionalities.

--- crm.rb
+++ crm.rb
@@ -26,6 +26,51 @@ require 'sinatra'
     end
   end
 
+  post '/contacts' do
+    Contact.create(
+      first_name: params[:first_name],
+      last_name:  params[:last_name],
+      email:      params[:email],
+      note:       params[:note]
+    )
+    redirect to('/contacts')
+  end
+
+  get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+  delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+  end
+
   not_found do
   "'Sinatra doesn't know this ditty...'"
   end
